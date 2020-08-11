0fdb2b3efe6db7654a74542612a74010368a291d
Damini Sheth <daminisheth98@gmail.com>
2020-08-11 20:07:20 +0530

    Navbar & Buttons

---
+++ src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+    <nav>
+        <v-app-bar flat class="indigo">
+            <v-app-bar-nav-icon @click="drawer = !drawer" class="white--text">
+            </v-app-bar-nav-icon>
+            <v-toolbar-title class="white--text">
+                <span class="font-weight-light">Todo</span>
+                <span>App</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn flat color="grey">
+                <span>Sign Out</span>
+            </v-btn>
+        </v-app-bar>
+        <v-navigation-drawer v-model="drawer" class="white" absolute temporary>
+            <v-list>
+                <v-list-item-group>
+                <v-list-item>
+                    <v-list-item-icon>
+                        <v-icon>mdi-home</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-title>Home</v-list-item-title>
+                </v-list-item>
+                 </v-list-item-group>
+            </v-list>
+           
+        </v-navigation-drawer>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data() {
+        return {
+            drawer: false
+        };
+    }
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
     <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png" />
-        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <!-- text, font classes on vuetify -->
+        <Buttons></Buttons>
+        
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Buttons from '@/components/Buttons';
 
 export default {
     name: 'Home',
     components: {
-        HelloWorld,
+        Buttons,
     },
 };
 </script>
