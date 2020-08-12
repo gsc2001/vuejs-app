8b9b73b268775350237dfb8032dde394dcf5629d
Gurkirat Singh <gurkirat.singh@students.iiit.ac.in>
2020-08-12 14:03:08 +0530

    [Try2] one more conflict

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
-Vue.use(Vuetify);
+Vue.use(Vuetify); // can i raise please
 
 // export default new Vuetify({});
 export default new Vuetify({}); // trying to raise a conflict
