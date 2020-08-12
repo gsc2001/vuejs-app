77c0284c66be3ad21223104b32275251c809ff02
Damini Sheth <63805708+damini98@users.noreply.github.com>
2020-08-12 14:00:16 +0530

    [Add] Navbar and Buttons Components (#2)

---
+++ src/components/Buttons.vue
@@ -0,0 +1,34 @@
+<template>
+    <!-- <v-app> -->
+    <div>
+        <h1>Buttons:</h1>
+        <p class="red white--text">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et
+            voluptas accusamus dolorum eveniet provident magni quae modi!
+            Excepturi veritatis atque fugiat, a sunt possimus necessitatibus
+            veniam, deserunt vero nemo praesentium.
+        </p>
+        <v-btn depressed color="blue">
+            <v-icon left>mdi-email</v-icon>
+            <span> Email Me!</span>
+        </v-btn>
+        <v-btn text depressed color="blue">
+            <v-icon>mdi-twitter</v-icon>
+        </v-btn>
+
+        <v-btn fab color="pink">
+            <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn color="green" class="hidden-sm-and-down">
+            visible > sm
+        </v-btn>
+    </div>
+    <!-- </v-app> -->
+</template>
+
+<script>
+export default {
+    name: 'Buttons'
+};
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,55 @@
+<template>
+    <nav>
+        <v-app-bar flat class="indigo">
+            <v-app-bar-nav-icon @click="drawer = !drawer" class="white--text">
+            </v-app-bar-nav-icon>
+            <v-toolbar-title class="white--text">
+                <span class="font-weight-light">Todo</span>
+                <span>App</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <div >
+                <router-link  to="/"> <span class="links"> Home      | </span> </router-link>
+                <router-link to="/about"> <span class="links"> About </span> </router-link>
+            </div>
+             <v-spacer></v-spacer>
+            <v-btn flat color="grey">
+                <span>Sign Out</span>
+            </v-btn>
+        </v-app-bar>
+        <v-navigation-drawer v-model="drawer" class="white" absolute temporary>
+            <v-list>
+                <v-list-item-group>
+                <v-list-item>
+                    <v-list-item-icon>
+                        <v-icon>mdi-home</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-title>Home</v-list-item-title>
+                </v-list-item>
+                 </v-list-item-group>
+            </v-list>
+           
+        </v-navigation-drawer>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data() {
+        return {
+            drawer: false
+        };
+    }
+};
+</script>
+
+<style>
+
+.links{
+    color: white;
+    font-size: 2rem;
+    text-decoration-line: none;
+}
+
+</style>
